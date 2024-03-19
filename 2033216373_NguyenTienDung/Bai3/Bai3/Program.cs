944e179a43745b57d8a9d44b987b25955ba3569f
nam <103032621+NamMaiNamMai0505@users.noreply.github.com>
2024-03-19 13:03:52 +0700

    Add files via upload

---
+++ 2033216373_NguyenTienDung/Bai3/Bai3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Dang ket noi voi server...");
+            //bài 4
+            try
+            {
+                serverSocket.Connect(serverEndPoint);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Khong the ket noi den server" + ex.ToString());
+                Console.ReadLine();
+                return;
+            }
+            //bài 6
+            if (serverSocket.Connected)
+            {
+
+              Console.WriteLine("Ket noi thanh cong voi server ...");
+                byte[] buff= new byte[1024];
+                int byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                string str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine(str);
+                Console.ReadLine();
+               
+
+            }
+           
+            
+        }
+    }
+}
---
+++ 2033216373_NguyenTienDung/Lab1/Bai1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //bai1
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+                serverSocket.Bind(serverEndPoint);
+     
+                serverSocket.Listen(1000);
+               
+         
+                Socket clientSocket = serverSocket.Accept();
+
+                EndPoint clientEndPoint = clientSocket.RemoteEndPoint;
+                Console.WriteLine(clientEndPoint.ToString());
+              
+            //bai 2
+                byte[] buff;
+                string hello = "Hello Client";
+                buff = Encoding.ASCII.GetBytes(hello);
+                clientSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+              
+            //bai 5
+                while (serverSocket.Connected==true)
+                {
+                    string str = Console.ReadLine();
+                    buff = Encoding.ASCII.GetBytes(str);
+                    serverSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+                    buff = new byte[1024];
+                    int byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                    str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                    Console.WriteLine(str);
+                   //IV.1 bao client mat ket noi 
+                    if (serverSocket.Connected == false)
+                    System.Console.WriteLine("Client mat ket noi");
+                    
+                }
+                Console.ReadLine();
+        }
+            
+    }
+}
